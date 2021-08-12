fea8544193a28d135b64291083c22d1e9b0b8b88
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-08-12 10:55:37 +0900

    Update

--- install/route.sh
+++ install/route.sh
@@ -10,7 +10,15 @@ echo "The application to make this pc faster."
 
 newsec
 
-echo "If you want to install, you need root privilege"
+echo "You can use without installing"
+echo "Do you want to install?"
+echo -e "1 - YES\n2 - No, Just run\n3 - No either"
+read choose
+if [ $choose == 1 ]{
+  echo "You need root privillege to install."
+  echo "Checking..."
+  sudo echo -n "OK"
+}
 # This is the command to make us can use sudo command.
 sudo echo "You are running on root privilege!"
 
